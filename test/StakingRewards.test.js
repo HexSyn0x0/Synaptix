67edbfe21a602df8b1d41551b95dcd7cd0b15595
HexSyn <Synaptix0x0@proton.me>
2025-12-07 19:01:04 +0100

    Update staking tests with reward rate and amounts

--- test/StakingRewards.test.js
+++ test/StakingRewards.test.js
@@ -5,28 +5,93 @@ describe("StakingRewards", function () {
   let token, staking;
   let deployer, user;
 
+  const initialUserBalance = ethers.utils.parseUnits("1000", 18);
+  const stakeAmount = ethers.utils.parseUnits("500", 18);
+  const rewardRate = ethers.utils.parseUnits("1", 18); // 1 SYNX per second
+
   beforeEach(async function () {
     [deployer, user] = await ethers.getSigners();
+
+    // Deploy token
     const Token = await ethers.getContractFactory("SynaptixToken");
     token = await Token.deploy();
     await token.deployed();
 
-    const Staking = await ethers.getContractFactory("StakingRewards");
-    staking = await Staking.deploy(token.address, token.address, 0);
-    await staking.deployed();
+    // Transfer initial balance to user
+    await token.transfer(user.address, initialUserBalance);
 
-    await token.transfer(user.address, 1000);
+    // Deploy StakingRewards
+    const Staking = await ethers.getContractFactory("StakingRewards");
+    staking = await Staking.deploy(token.address, token.address, rewardRate);
+    await staking.deployed();
   });
 
   it("user can stake tokens", async function () {
-    await token.connect(user).approve(staking.address, 1000);
-    await staking.connect(user).stake(1000);
+    await token.connect(user).approve(staking.address, stakeAmount);
+    await staking.connect(user).stake(stakeAmount);
+
     const balance = await staking.balanceOf(user.address);
-    expect(balance).to.equal(1000);
+    const totalSupply = await staking.totalSupply();
+    expect(balance).to.equal(stakeAmount);
+    expect(totalSupply).to.equal(stakeAmount);
   });
 
   it("user cannot stake more than balance", async function () {
-    await token.connect(user).approve(staking.address, 2000);
-    await expect(staking.connect(user).stake(2000)).to.be.reverted;
+    const tooMuch = ethers.utils.parseUnits("2000", 18);
+    await token.connect(user).approve(staking.address, tooMuch);
+    await expect(staking.connect(user).stake(tooMuch)).to.be.revertedWith(
+      "ERC20: transfer amount exceeds balance"
+    );
+  });
+
+  it("user can withdraw staked tokens", async function () {
+    await token.connect(user).approve(staking.address, stakeAmount);
+    await staking.connect(user).stake(stakeAmount);
+
+    await staking.connect(user).withdraw(stakeAmount);
+
+    const balance = await staking.balanceOf(user.address);
+    const totalSupply = await staking.totalSupply();
+    expect(balance).to.equal(0);
+    expect(totalSupply).to.equal(0);
+  });
+
+  it("user can claim rewards", async function () {
+    await token.connect(user).approve(staking.address, stakeAmount);
+    await staking.connect(user).stake(stakeAmount);
+
+    // simulate some time passing
+    await ethers.provider.send("evm_increaseTime", [10]); // 10 seconds
+    await ethers.provider.send("evm_mine");
+
+    const rewardsBefore = await staking.earned(user.address);
+    expect(rewardsBefore).to.be.gt(0);
+
+    const userBalanceBefore = await token.balanceOf(user.address);
+    await staking.connect(user).getReward();
+    const userBalanceAfter = await token.balanceOf(user.address);
+
+    expect(userBalanceAfter.sub(userBalanceBefore)).to.equal(rewardsBefore);
+  });
+
+  it("user can exit (withdraw + claim rewards)", async function () {
+    await token.connect(user).approve(staking.address, stakeAmount);
+    await staking.connect(user).stake(stakeAmount);
+
+    await ethers.provider.send("evm_increaseTime", [10]);
+    await ethers.provider.send("evm_mine");
+
+    const rewardsBefore = await staking.earned(user.address);
+    const userBalanceBefore = await token.balanceOf(user.address);
+
+    await staking.connect(user).exit();
+
+    const userBalanceAfter = await token.balanceOf(user.address);
+    const stakedBalance = await staking.balanceOf(user.address);
+    const totalSupply = await staking.totalSupply();
+
+    expect(stakedBalance).to.equal(0);
+    expect(totalSupply).to.equal(0);
+    expect(userBalanceAfter.sub(userBalanceBefore)).to.equal(stakeAmount.add(rewardsBefore));
   });
 });
