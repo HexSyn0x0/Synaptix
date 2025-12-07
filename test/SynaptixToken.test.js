23095b4d12c7575501c2c02c527fd69f7f71d7b4
HexSyn <Synaptix0x0@proton.me>
2025-12-07 19:03:32 +0100

    Update SynaptixToken tests for total supply and transfers

--- test/SynaptixToken.test.js
+++ test/SynaptixToken.test.js
@@ -5,26 +5,49 @@ describe("SynaptixToken", function () {
   let token;
   let deployer, user;
 
+  const TOTAL_SUPPLY = ethers.utils.parseUnits("100000000", 18); // 100M SYNX
+
   beforeEach(async function () {
     [deployer, user] = await ethers.getSigners();
+
     const Token = await ethers.getContractFactory("SynaptixToken");
     token = await Token.deploy();
     await token.deployed();
   });
 
-  it("should assign total supply to deployer", async function () {
-    const totalSupply = await token.totalSupply();
+  it("should assign the total supply to the deployer", async function () {
     const deployerBalance = await token.balanceOf(deployer.address);
-    expect(deployerBalance).to.equal(totalSupply);
+    expect(deployerBalance).to.equal(TOTAL_SUPPLY);
   });
 
   it("should transfer tokens correctly", async function () {
-    await token.transfer(user.address, 1000);
-    expect(await token.balanceOf(user.address)).to.equal(1000);
+    const transferAmount = ethers.utils.parseUnits("1000", 18);
+    await token.transfer(user.address, transferAmount);
+    const userBalance = await token.balanceOf(user.address);
+    expect(userBalance).to.equal(transferAmount);
+
+    const deployerBalance = await token.balanceOf(deployer.address);
+    expect(deployerBalance).to.equal(TOTAL_SUPPLY.sub(transferAmount));
   });
 
   it("should not allow transferring more than balance", async function () {
-    await expect(token.transfer(user.address, ethers.utils.parseUnits("100000000", 18)))
-      .to.be.reverted;
+    const excessiveAmount = TOTAL_SUPPLY.add(ethers.utils.parseUnits("1", 18));
+    await expect(token.transfer(user.address, excessiveAmount)).to.be.revertedWith(
+      "ERC20: transfer amount exceeds balance"
+    );
+  });
+
+  it("should allow multiple sequential transfers", async function () {
+    const amount1 = ethers.utils.parseUnits("5000", 18);
+    const amount2 = ethers.utils.parseUnits("2000", 18);
+
+    await token.transfer(user.address, amount1);
+    await token.transfer(user.address, amount2);
+
+    const userBalance = await token.balanceOf(user.address);
+    expect(userBalance).to.equal(amount1.add(amount2));
+
+    const deployerBalance = await token.balanceOf(deployer.address);
+    expect(deployerBalance).to.equal(TOTAL_SUPPLY.sub(amount1.add(amount2)));
   });
 });
